42fc6aee2bc5c409100c7960f0fe42e3d3b308d7
Daniel King <dan@spiraldb.com>
2024-11-13 21:16:16 -0500

    restore from.rs

--- vortex-array/src/array/from.rs
+++ vortex-array/src/array/from.rs
@@ -42,24 +42,11 @@ impl_from_primitive_for_array!(f16);
 impl_from_primitive_for_array!(f32);
 impl_from_primitive_for_array!(f64);
 
-macro_rules! impl_from_string_like {
-    ($T:ty) => {
-        impl From<Vec<$T>> for Array {
-            fn from(value: Vec<$T>) -> Self {
-                VarBinViewArray::from_iter_bin(value).into_array()
-            }
-        }
-
-        impl From<Vec<Option<$T>>> for Array {
-            fn from(value: Vec<Option<$T>>) -> Self {
-                VarBinViewArray::from_iter(value, DType::Utf8(Nullability::Nullable)).into_array()
-            }
-        }
-    };
+impl From<Vec<Option<BufferString>>> for Array {
+    fn from(value: Vec<Option<BufferString>>) -> Self {
+        VarBinViewArray::from_iter(value, DType::Utf8(Nullability::Nullable)).into_array()
+    }
 }
-
-impl_from_string_like!(String);
-impl_from_string_like!(BufferString);
 
 impl From<Vec<Option<Buffer>>> for Array {
     fn from(value: Vec<Option<Buffer>>) -> Self {
