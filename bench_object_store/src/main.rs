05f103ad1b7e8f0b929eaea0f753c726d87d01ef
Andrew Duffy <andreweduffy@gmail.com>
2024-11-20 14:56:01 -0500

    create repo

---
+++ bench_object_store/src/main.rs
@@ -0,0 +1,6 @@
+use vortex::file::{VortexFileArrayStream, VortexReadBuilder};
+
+// We want to have fine-grained control over the runtime when we record our measurements here.
+fn main() {
+    VortexReadBuilder::new()
+}
